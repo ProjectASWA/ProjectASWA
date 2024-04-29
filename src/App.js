ad06295faea60cba171f8ea245777af231bac1e7
Chennakiran2004 <kirankumarchenna2004@gmail.com>
2024-04-29 09:36:53 +0530

    NavBar Component Added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./styles.css";
+import NavBar from "./components/NavBar";
 
 export default function App() {
   return (
@@ -6,6 +7,7 @@ export default function App() {
       <h1>Hello World</h1>
       <h1>Hello CodeSandbox</h1>
       <h2>Start editing to see some magic happen!</h2>
+      <NavBar />
     </div>
   );
 }
---
+++ src/components/NavBar/index.js
@@ -0,0 +1,9 @@
+import "./index.css";
+
+export default function NavBar() {
+  return (
+    <div className="nav-bar">
+      <h1 className="nav-bar-title">NavBar</h1>
+    </div>
+  );
+}
