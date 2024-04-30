8cc85aedc44474632779eaf84ad2b26ffc26f8cd
Chennakiran2004 <kirankumarchenna2004@gmail.com>
2024-04-30 14:58:54 +0530

    Testimonial

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -6,15 +6,17 @@ import CSRPartners from "../CSRPartners";
 
 import Footer from "../Footer";
 
-import Impact from '../OurImpact'
+import Impact from "../OurImpact";
+import Testimonials from "../Testimonials";
 
 const Home = () => {
   return (
     <>
       <CarouselImages />
       <OurProjects />
-      <Impact/>
+      <Impact />
       <CSRPartners />
+      <Testimonials />
       <Footer />
     </>
   );
---
+++ src/components/TestimonialsCard/index.js
@@ -0,0 +1,57 @@
+import "./index.css";
+
+import { Component } from "react";
+
+const TestimonialsCard = (props) => {
+  return (
+    <div className="TestimonialsCard-app-container">
+      <div className="TestimonialsCard-container">
+        <img
+          src={"/Images/TanikellaBharani.png"}
+          alt="image"
+          className="TestimonialsCard-image"
+        />
+        <div className="TestimonialsCard-text">
+          <img
+            className="double-quotes-image"
+            src={"/Images/double-quotes-l.1024x786.png"}
+          />
+          <p className="TestimonialsCard-description">
+            ASWA is doing a great service by motivating govt. school children to
+            have affection and love towards arts, education, life and created a
+            platform to exhibit their skills. ASWA started like a drop of water
+            7yrs ago, is currently flowing like a canal and can turn into a
+            river & spreads out vastly.*”
+          </p>
+          <h1 className="TestimonialsCard-name">
+            - Shri Tanikella Bharani Actor, Poet & Director
+          </h1>
+        </div>
+      </div>
+      <hr />
+      <div className="TestimonialsCard-container">
+        <div className="TestimonialsCard-image">
+          <img src={"/Images/TanikellaBharani.png"} alt="image" />
+        </div>
+        <div className="TestimonialsCard-text">
+          <img
+            className="double-quotes-image"
+            src={"/Images/double-quotes-l.1024x786.png"}
+          />
+          <p className="TestimonialsCard-description">
+            ASWA is doing a great service by motivating govt. school children to
+            have affection and love towards arts, education, life and created a
+            platform to exhibit their skills. ASWA started like a drop of water
+            7yrs ago, is currently flowing like a canal and can turn into a
+            river & spreads out vastly.*”
+          </p>
+          <h1 className="TestimonialsCard-name">
+            - Shri Tanikella Bharani Actor, Poet & Director
+          </h1>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default TestimonialsCard;
