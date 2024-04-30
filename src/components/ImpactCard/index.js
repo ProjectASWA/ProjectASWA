ad50c4d500f1fca1216ff755aba69f3a8e8fe792
vakitimanoj <mahimanoj083@gmail.com>
2024-04-30 11:32:41 +0530

    Count Feature Added

--- src/components/ImpactCard/index.js
+++ src/components/ImpactCard/index.js
@@ -5,9 +5,13 @@ import './index.css'
 const ImpactCard = (props)=>{
     const {details} = props
     const {image} = details
+
+    const constantEasing = (t) => {
+    }
+
     return(
         <div className = "backgroundStylingImage" style={{backgroundImage: `url(${image})`}}>
-           <h1><CountUp start={0} end={details.count} duration={5}/>
+           <h1><CountUp  start = {500} end={details.count} duration={1}/>
             K+</h1>
            <p className='descriptionStyling'>{details.description}</p>
         </div>
