459f48a125b70436fdbcf00f7b8a0f07b04a66b2
Chennakiran2004 <kirankumarchenna2004@gmail.com>
2024-05-01 16:39:51 +0530

    Edcuation

--- src/components/EducationCards/index.js
+++ src/components/EducationCards/index.js
@@ -23,73 +23,74 @@ const EducationCards = () => {
       </div>
       <div className="card-app-container">
         <div className="card-image">
-          <img src="/Images/schooleducation1.png" alt="Education" />
+          <img src="Images/schooleducation2lg.png" alt="Education" />
         </div>
         <div className="card-description-container">
           <h1 className="card-description-heading">
-            Foundational Literacy and Numeracy (FLN)
+            Children Learning Center (CLC)
           </h1>
           <p className="card-description">
-            Foundational literacy and numeracy(FLN): Aims to strengthen and
-            improve under-resourced schools and underprivileged communities with
-            a specific focus on literacy and numeracy competencies in children
-            using innovative teaching and learning methods. To achieve higher
-            levels of efficiency in FLN delivery we are using Maxine Bernstein's
-            "Balanced Approach".
+            CLC started as a pandemic pet project in October 2020, for the
+            children of the Ram Nagar Slum of Shadnagar. It served the purpose
+            of an after-school learning center so that children did not lose the
+            flow of schooling & education post-pandemic. Presently, CLC helps in
+            engaging children even when school is not running, prevents
+            dropouts, generates curiosity in children, and facilitates their
+            learning process. At CLC, we encourage them to express their ideas
+            and thoughts and learn the language using stories, rhymes, games,
+            drawing, and other activities. We teach them language and literacy
+            using their day-to-day life experiences. Around60+ children are
+            benefiting from this.
           </p>
         </div>
       </div>
       <div className="card-app-container">
         <div className="card-image">
-          <img src="/Images/schooleducation1.png" alt="Education" />
+          <img src="/Images/schooleducation3lg.png" alt="Education" />
         </div>
         <div className="card-description-container">
-          <h1 className="card-description-heading">
-            Foundational Literacy and Numeracy (FLN)
-          </h1>
+          <h1 className="card-description-heading">Children’s Library</h1>
           <p className="card-description">
-            Foundational literacy and numeracy(FLN): Aims to strengthen and
-            improve under-resourced schools and underprivileged communities with
-            a specific focus on literacy and numeracy competencies in children
-            using innovative teaching and learning methods. To achieve higher
-            levels of efficiency in FLN delivery we are using Maxine Bernstein's
-            "Balanced Approach".
+            Libraries and Children's literature (LCL) commenced with the motive
+            to inculcate the value of storytelling in first-generation learners.
+            The objective of the Children's Library is to create a love for book
+            reading and to introduce creative & critical thinking, expression,
+            narration, visualization, and reading, questioning, and improving
+            communication skills, and providing exposure to varied perspectives
+            to children coming from marginalized communities.
           </p>
         </div>
       </div>
       <div className="card-app-container">
         <div className="card-image">
-          <img src="/Images/schooleducation1.png" alt="Education" />
+          <img src="/Images/schooleducation4lg.png" alt="Education" />
         </div>
         <div className="card-description-container">
-          <h1 className="card-description-heading">
-            Foundational Literacy and Numeracy (FLN)
-          </h1>
+          <h1 className="card-description-heading">Career Guidance</h1>
           <p className="card-description">
-            Foundational literacy and numeracy(FLN): Aims to strengthen and
-            improve under-resourced schools and underprivileged communities with
-            a specific focus on literacy and numeracy competencies in children
-            using innovative teaching and learning methods. To achieve higher
-            levels of efficiency in FLN delivery we are using Maxine Bernstein's
-            "Balanced Approach".
+            Our Career Guidance program was launched to spread awareness about
+            the career options available after class 10th to secondary school
+            students. Volunteers conduct sessions on personality, goal setting,
+            and career options available after the 10th class. Post the
+            sessions, every attendee gets a unique guide designed by ASWA that
+            offers clarity on choosing a career path for the next 6-7 years. 
           </p>
         </div>
       </div>
       <div className="card-app-container">
         <div className="card-image">
-          <img src="/Images/schooleducation1.png" alt="Education" />
+          <img src="/Images/schooleducation5lg.png" alt="Education" />
         </div>
         <div className="card-description-container">
-          <h1 className="card-description-heading">
-            Foundational Literacy and Numeracy (FLN)
-          </h1>
+          <h1 className="card-description-heading">Project Cheyutha</h1>
           <p className="card-description">
-            Foundational literacy and numeracy(FLN): Aims to strengthen and
-            improve under-resourced schools and underprivileged communities with
-            a specific focus on literacy and numeracy competencies in children
-            using innovative teaching and learning methods. To achieve higher
-            levels of efficiency in FLN delivery we are using Maxine Bernstein's
-            "Balanced Approach".
+            Financial Support and Guidance to Meritorious Students: Project
+            Cheyutha was launched to offer scholarships to meritorious students
+            coming from economically deprived families. Students who aim to
+            pursue higher education (beyond Grade 10) are eligible for the
+            scholarship. Through Project Cheyutha, we generally extend support
+            for hostel, mess, tuition & stationery fees. Till now we have
+            extended support to over 65+ students. 
           </p>
         </div>
       </div>
