0241f6631ba252bd72244fc88bf7719241a44041
vakitimanoj <mahimanoj083@gmail.com>
2024-05-03 19:21:58 +0530

    Added Changes to the home page

--- src/components/Carousel/index.js
+++ src/components/Carousel/index.js
@@ -6,22 +6,34 @@ import "./index.css";
 const CarouselImages = () => {
   return (
     <Carousel
-      autoPlay
+      
       interval={2000}
       infiniteLoop
       dynamicHeight
       showStatus={false}
     >
-      <div className="customCarousel">
-        <h1>Image 1</h1>
+      <div className="customCarousel1">
+          <div className="customCarouselTextContainer">
+          <h1 className="carouselHeading">Building Brighter Futures</h1>
+              <p className="carouselDescription">Providing quality education and career guidance to unlock potential and create opportunities.</p>
+              <button className = "knowMoreCarousel">Know More</button>
+          </div>
       </div>
-      <div className="customCarousel">
-        <h1>Image 2</h1>
+      <div className="customCarousel2">
+        <div className="customCarouselTextContainer">
+            <h1 className="carouselHeading secondBannerHeading">Be a Lifesaver</h1>
+                <p className="carouselDescription secondBannerSideHeading">Join our blood donation program and help ensure vital blood supplies for those in need.</p>
+                <button className = "knowMoreCarousel">Know More</button>
+        </div>
       </div>
-      <div className="customCarousel">
-        <h1>Image 3</h1>
+      <div className="customCarousel3">
+        <div className="customCarouselTextContainer">
+          <h1 className="carouselHeading carousel3Heading">Greening Our Future</h1>
+              <p className="carouselDescription carousel3SideHeading">Promoting environmental stewardship and conservation for a healthier, more sustainable world.</p>
+              <button className = "knowMoreCarousel">Know More</button>
+          </div>
       </div>
-      <div className="customCarousel">
+      <div className="customCarousel4">
         <h1>Image 4</h1>
       </div>
       <div className="customCarousel">
