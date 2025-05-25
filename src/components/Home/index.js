f8ec0fd807e03b2f6c8af2b5abf732755773a6b1
Rajesh Kongathi <konga@live.in>
2025-05-25 13:23:54 +0530

    auto scroll little bit

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,22 +1,29 @@
+import { useEffect } from "react";
 import CarouselImages from "../Carousel";
-
 import CSRPartners from "../CSRPartners";
-
 import Impact from "../OurImpact";
-
 import Testimonials from "../Testimonials";
-
 import MobileTestimonials from "../MobileTestimonials";
-
 import AboutUsHomeSection from "../AboutUsHome";
-
 import OurProjects1 from "../OurProjects1";
-
 import "./index.css";
 
 // import AnimatedFlexCards from "../AnimatedFlexCards";
 
 const Home = () => {
+  useEffect(() => {
+      // Scroll down smoothly by window height after page load
+      window.scrollTo({ top: window.innerHeight, behavior: "smooth" });
+
+      // After the scroll down is complete, scroll back to top
+      const timeout = setTimeout(() => {
+        window.scrollTo({ top: 0, behavior: "smooth" });
+      }, 900); // Adjust time as needed for your animation
+
+      // Cleanup timeout on unmount
+      return () => clearTimeout(timeout);
+    }, []);  
+
   return (
     <div className="home-container">
       <CarouselImages />
